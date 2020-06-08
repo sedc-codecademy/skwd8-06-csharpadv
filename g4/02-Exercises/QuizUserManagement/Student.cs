1a1113fa127261cc5804f7a5de0a29336d81f66d
Wekoslav Stefanovski <swekster@gmail.com>
2020-06-08 18:39:30 +0200

    Added login

---
+++ g4/02-Exercises/QuizUserManagement/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuizUserManagement
+{
+    public class Student: User
+    {
+        public bool FinishedQuiz { get; set; }
+        public int? Score { get; set; }
+
+        internal Student()
+        {
+
+        }
+    }
+}
---
+++ g4/02-Exercises/QuizUserManagement/UserRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QuizUserManagement
+{
+    public class UserRepository
+    {
+        private readonly List<User> users = new List<User>();
+
+        public User? Login(string username, string password)
+        {
+            var candidateUser = users.SingleOrDefault(user => user.UserName == username);
+            if (candidateUser == null)
+            {
+                return null;
+            }
+            if (candidateUser.Password == password)
+            {
+                return candidateUser;
+            }
+            else
+            {
+                return null;
+            }
+        }
+ 
+        public UserRepository()
+        {
+            users.Add(new Student { FirstName = "Aleksandar", LastName = "Jangelovski", UserName = "alex", Password = "alex" });
+            users.Add(new Student { FirstName = "Petre", LastName = "Arsovski", UserName = "petre", Password = "petre" });
+            users.Add(new Student { FirstName = "Blagoja", LastName = "Grozdanovski", UserName = "bazejbt", Password = "bazejbt" });
+            users.Add(new Student { FirstName = "Deni", LastName = "Stojanovski", UserName = "deni", Password = "deni" });
+            users.Add(new Student { FirstName = "Katerina", LastName = "Atanasoska", UserName = "kate", Password = "kate" });
+
+            users.Add(new Teacher { FirstName = "Wekoslav", LastName = "Stefanovski", UserName = "swekster", Password = "swekster" });
+            users.Add(new Teacher { FirstName = "Kristina", LastName = "Spasevska", UserName = "kiki", Password = "kiki" });
+        }
+
+    }
+}
