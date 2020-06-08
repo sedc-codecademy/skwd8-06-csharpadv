cd05291584293a09998e26e34d9197077a15db97
Dragan Gelevski <hello@dragan.codes>
2020-06-08 20:32:14 +0200

    Class 03 finito

---
+++ g7/Class-01/SEDC.Adv.01/SEDC.Adv.01.AbstractClasses/Entities/DevOps.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.Adv._01.AbstractClasses.Entities
+{
+	// This class implements 2 interfaces because the DevOps role requires both Developer and Operations features
+	public class DevOps : Human, IDeveloper, IOperations
+	{
+		public bool AWSCertified { get; set; }
+		public bool AzureCertified { get; set; }
+		public DevOps(string fullName, int age, long phone, bool aws, bool azure)
+			: base(fullName, age, phone)
+		{
+			AWSCertified = aws;
+			AzureCertified = azure;
+		}
+		public bool CheckInfrastructure(int status)
+		{
+			List<int> okStatuses = new List<int>() { 200, 202, 204 };
+			if (okStatuses.Contains(status)) return true;
+			return false;
+		}
+
+		public void Code()
+		{
+			Console.WriteLine("Tak tak tak...");
+			Console.WriteLine("*Opens StackExchange DevOps...");
+			Console.WriteLine("Tak tak tak tak tak...");
+		}
+
+		public override string GetInfo()
+		{
+			string result = $"{FullName} ({Age}) - Has:";
+			// Short for if/else or Ternary operator
+			// If the side before the ? is true, then return the left side of the :
+			// If the side beore the ? is false, then return the right side of the :
+			result += AWSCertified ? " AWS Certificate" : "";
+			result += AzureCertified ? " Azure Certificate" : "";
+			// && AND -> Will check if at least one of them is FALSE, so the whole thing will be FALSE
+			// || OR -> Will check if at least one is TRUE , so that the whole thing will be TRUE
+			// true || false -> ""
+			// false || true -> ""
+			// true || true -> "" ( Has both certificates )
+			// false || false -> "No certifiates yet"
+			result += AWSCertified || AzureCertified ? "" : "No certificates yet";
+			result += AWSCertified && AzureCertified ? "ALL THE CERTIFICATES" : "";
+			return result;
+		}
+	}
+}
--- g7/Class-01/SEDC.Adv.01/SEDC.Adv.01.AbstractClasses/Program.cs
+++ g7/Class-01/SEDC.Adv.01/SEDC.Adv.01.AbstractClasses/Program.cs
@@ -17,10 +17,40 @@ namespace SEDC.Adv._01.AbstractClasses
 			Tester tester = new Tester("Jill Wayne", 32, 38971071071, 560);
 			Operations ops = new Operations("Greg Gregsky", 28, 38975075075,
 				new List<string>() { "Optimus", "ProtoBeat", "PickPro" });
-
+			DevOps devOps = new DevOps("Anne Brown", 24, 38977070070, true, false);
+			QAEngineer qa = new QAEngineer("Mia Wong", 34, 38972072072,
+				new List<string>() { "Selenium" });
+			Console.WriteLine("WELCOME TO THE COMPANY APP");
+			Console.WriteLine("--------------------------");
+			Console.WriteLine("Developer:");
 			Console.WriteLine(dev.GetInfo());
+			dev.Greet("Students");
+			dev.Code();
+			Console.WriteLine("--------------------------");
+			Console.WriteLine("Tester:");
 			Console.WriteLine(tester.GetInfo());
+			tester.Greet("Students");
+			tester.TestFeature("Log in");
+			Console.WriteLine("--------------------------");
+			Console.WriteLine("IT Operations:");
 			Console.WriteLine(ops.GetInfo());
+			ops.Greet("Students");
+			Console.WriteLine($"Infrastructure OK: {ops.CheckInfrastructure(200)}");
+			Console.WriteLine("--------------------------");
+			Console.WriteLine("QA Engineer:");
+			Console.WriteLine(qa.GetInfo());
+			qa.Greet("Students");
+			qa.TestFeature("Log out");
+			qa.Code();
+			Console.WriteLine("--------------------------");
+			Console.WriteLine("Dev Ops:");
+			Console.WriteLine(devOps.GetInfo());
+			devOps.Greet("Students");
+			Console.WriteLine($"Infrastructure OK: {devOps.CheckInfrastructure(404)}");
+			devOps.Code();
+
+
+
 
 			Console.ReadLine();
 		}
