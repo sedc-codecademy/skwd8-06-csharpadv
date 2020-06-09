1ba1912632add0ff875c6f2eed18af28dd23c443
Wekoslav Stefanovski <swekster@gmail.com>
2020-06-09 21:58:30 +0200

    Added author starter

---
+++ g4/02-Exercises/AuthorStarter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace AuthorStarter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var repo = new AuthorRepo();
+            var authors = repo.GetAuthors();
+            Console.WriteLine($"There are {authors.Count()} total authors");
+            Console.WriteLine($"with {authors.SelectMany(a => a.Books).Count()} total books");
+
+            Console.WriteLine("How many books are collaborations (have more than one author)?");
+            Console.WriteLine("Which book has the most authors (and how many)?");
+            Console.WriteLine("What author wrote most collaborations?");
+        }
+    }
+}
