dfb140fa9d5f0163a8e84460b14e1588961c2f29
Andrej Chichakovski <Andrej.Chichakovski@seavus.com>
2020-06-10 19:59:36 +0200

    Exercise solution

---
+++ g5/Class03/Adv.Class03.Exercise/Adv.Class03.Exercise/Entities/Student.cs
@@ -0,0 +1,25 @@
+﻿using Adv.Class03.Exercise.Entities.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Adv.Class03.Exercise.Entities
+{
+	public class Student : User, IStudent
+	{
+		public List<int> Grades { get; set; }
+		public void PringGrades()
+		{
+			foreach (int grade in Grades)
+			{
+				Console.WriteLine(grade);
+			}
+		}
+
+		public override void PrintUser()
+		{
+			Console.WriteLine($"Student: {Name} with username {Username} and average grade of {Grades.Sum() / Grades.Count}");
+		}
+	}
+}
---
+++ g5/Class03/Adv.Class03.Exercise/Adv.Class03.Exercise/Entities/Teacher.cs
@@ -0,0 +1,25 @@
+﻿using Adv.Class03.Exercise.Entities.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Adv.Class03.Exercise.Entities
+{
+	public class Teacher : User, ITeacher
+	{
+		public List<string> Subjects { get; set; }
+
+		public void PrintSubjects()
+		{
+			foreach (string subject in Subjects)
+			{
+				Console.WriteLine(subject);
+			}
+		}
+
+		public override void PrintUser()
+		{
+			Console.WriteLine($"Teacher: {Name} with username {Username} that teaches {Subjects.Count} subjecs");
+		}
+	}
+}
