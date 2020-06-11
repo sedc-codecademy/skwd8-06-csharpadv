65d1e2f6ea28f89e6c602cea7d27acc64a626bf4
Goce Kabov <Goce.Kabov@seavus.com>
2020-06-11 18:42:56 +0200

    added class 04

---
+++ g1/Class04/Adv.Class02/Adv.Class02.Polymorphism/Entities/Dog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Adv.Class03.Polymorphism.Entities
+{
+	public class Dog : Pet
+	{
+		public bool IsGoodBoi { get; set; }
+		public override void Eat()
+		{
+			Console.WriteLine("Nom nom noming on dog food!");
+		}
+	}
+}
