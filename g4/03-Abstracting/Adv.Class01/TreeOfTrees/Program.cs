11c87e6d006b407ad20bc69f12a9261bb62f6310
Wekoslav Stefanovski <swekster@gmail.com>
2020-06-12 18:42:51 +0200

    added tree of trees demo

---
+++ g4/03-Abstracting/Adv.Class01/TreeOfTrees/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Xml.XPath;
+
+namespace TreeOfTrees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var weko = new Person { FirstName = "Wekoslav", LastName = "Stefanovski" };
+            Console.WriteLine(weko.GetType().FullName);
+            Console.WriteLine(weko);
+            Console.WriteLine("------------");
+
+
+            Tree myTree = new AppleTree();
+            var result = myTree.GetSeedsMultiple(3);
+            Console.WriteLine(string.Join("\n", result));
+
+        }
+    }
+}
---
+++ g4/03-Abstracting/Adv.Class01/TreeOfTrees/Tree.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TreeOfTrees
+{
+    public abstract class Tree
+    {
+        public string Trunk { get; set; }
+        public string Root { get; set; }
+        public List<string> Branches { get; set; }
+
+        public abstract string GetSeeds();
+
+        public List<string> GetSeedsMultiple(int times)
+        {
+            List<string> result = new List<string>();
+            for (int i = 0; i < times; i++)
+            {
+                result.Add(GetSeeds());
+            }
+            return result;
+        }
+    }
+
+    public abstract class FriutTree : Tree
+    {
+        public string FruitName { get; private set; }
+        public FriutTree(string fruitName)
+        {
+            FruitName = fruitName;
+        }
+    }
+
+    public class AppleTree : FriutTree
+    {
+        public AppleTree() : base("Apples")
+        {
+
+        }
+
+        public override string GetSeeds()
+        {
+            return FruitName;
+        }
+
+    }
+
+}
