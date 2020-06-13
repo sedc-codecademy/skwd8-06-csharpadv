d917f593e7193bf657cc3e0300887980134e8b26
Dragan Gelevski <hello@dragan.codes>
2020-06-13 10:06:36 +0200

    Class 05 - pt2

--- g7/Class-02/SEDC.Adv.02/SEDC.Adv.02.Polymorphism/Entities/Cat.cs
+++ g7/Class-02/SEDC.Adv.02/SEDC.Adv.02.Polymorphism/Entities/Cat.cs
@@ -7,7 +7,7 @@ namespace SEDC.Adv._02.Polymorphism.Entities
 	public class Cat : Pet
 	{
 		public bool IsLazy { get; set; }
-
+		// Dog also has Eat() and it is has the same signature
 		public override void Eat()
 		{
 			Console.WriteLine("Nom nom nomming on cat food!");
