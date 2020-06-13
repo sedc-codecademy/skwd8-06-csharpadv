cfbc64bceebfc86004608d1733c7be077a19fe89
Dragan Gelevski <hello@dragan.codes>
2020-06-13 09:24:58 +0200

    Class 05 - pt1

---
+++ g7/Class-02/SEDC.Adv.02/SEDC.Adv.02.Polymorphism/Entities/Dog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.Adv._02.Polymorphism.Entities
+{
+	public class Dog : Pet
+	{
+		public bool IsGoodBoi { get; set; }
+
+		public override void Eat()
+		{
+			Console.WriteLine("Nom nom nom nomming dog food!");
+		}
+	}
+}
---
+++ g7/Class-02/SEDC.Adv.02/SEDC.Adv.02.Polymorphism/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SEDC.Adv._02.Polymorphism
+{
+	class Program
+	{
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
