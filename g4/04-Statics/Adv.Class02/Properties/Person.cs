507cc815f98f74eb8b9c77706b7ef69be82229f9
Wekoslav Stefanovski <swekster@gmail.com>
2020-06-13 10:30:06 +0200

    Added custom getters

---
+++ g4/04-Statics/Adv.Class02/Properties/Person.cs
@@ -0,0 +1,55 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Properties
+{
+    public class Person
+    {
+        private string firstName;
+
+        public string GetFirstName()
+        {
+            return firstName;
+        }
+
+        public void SetFirstName(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentException("Value cannot be null or empty");
+            }
+            firstName = value;
+        }
+
+        public string LastName;
+
+        private int age;
+
+        public int GetAge()
+        {
+            return age;
+        }
+
+        public void SetAge(int value)
+        {
+            age = value;
+        }
+
+        public int Age
+        {
+            get 
+            {
+                return age;
+            }
+            set 
+            {
+                age = value;
+            }
+        }
+
+
+    }
+}
---
+++ g4/04-Statics/Adv.Class02/Properties/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person weko = new Person();
+            weko.SetAge(42);
+            weko.SetFirstName("Wekoslav");
+            weko.LastName = "Stefanovski";
+            Console.WriteLine($"{weko.GetFirstName()} {weko.LastName}");
+
+            weko.SetAge(weko.GetAge() + 1);
+            Console.WriteLine(weko.GetAge());
+
+            weko.Age += 1;
+            Console.WriteLine(weko.Age);
+       }
+    }
+}
