d790e59d94d4777224d7fa0f0ecfd75c2e4ec55f
Andrej Chichakovski <Andrej.Chichakovski@seavus.com>
2020-06-14 15:56:00 +0200

    Exercise solution

---
+++ g5/Class04/Class04.Exercise/Class04.Exercise/Shelter/Dog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class04.Exercise
+{
+	public class Dog
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Color { get; set; }
+		public void Bark()
+		{
+			Console.WriteLine("Bark Bark");
+		}
+		public static bool Validate(Dog dog)
+		{
+			if (dog.Id <= 0 || dog.Name.Length < 2 || dog.Color == "") return false;
+			return true;
+		}
+	}
+}
