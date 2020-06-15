1b909fd93b6a6a61cc703ed71cf707742029b2c7
Dragan Gelevski <hello@dragan.codes>
2020-06-15 20:22:49 +0200

    Class 06 - pt3

---
+++ g7/TryBeingFitApp/SEDC.Adv.TryBeingFit/SEDC.Adv.TryBeingFit.Domain/Core/Interfaces/ITrainerUser.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.Adv.TryBeingFit.Domain
+{
+    public interface ITrainerUser
+    {
+        bool ChangeSchedule(LiveTraining liveTraining);
+    }
+}
