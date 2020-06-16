70945674b17b870746ca16f886d44b50100beee9
Александар Коцевски <alek.kocevski@hotmail.com>
2020-06-16 19:02:31 +0200

    Polymorphism

---
+++ g3/Class-04/Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            //Getters and setters explanation
+            //Square square = new Square(4);
+            //square.Length = 5;
+            //double lengthOfThisSquare = square.Length;
+
+            //Dynamic Polymorphism
+            Circle circle = new Circle(3);
+            Square square = new Square(5);
+            Rectangle rectangle = new Rectangle(4, 5);
+
+            //circle.Area();
+            //square.Area();
+            //rectangle.Area();
+
+            List <Shape> shapes = new List<Shape>();
+            shapes.Add(circle);
+            shapes.Add(square);
+            shapes.Add(rectangle);
+
+            foreach (var shape in shapes)
+            {
+                shape.Area();
+            }
+
+            //Static Polymorphism
+            //int x, string y != string x, int y ... as parameters
+            circle.Area();
+            circle.Area(4);
+
+            rectangle.Area();
+            rectangle.Area(9);
+            rectangle.Area(5, 6);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ g3/Class-04/Polymorphism/Polymorphism/Shapes.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Polymorphism
+{
+    public class Shape :Object
+    {
+        public virtual void Area()
+        {
+            Console.WriteLine($"This is the base method calling");
+        }
+        public virtual void Area(double x)
+        {
+            Console.WriteLine($"This is the base method calling with 1 parameter");
+        }
+        public virtual void Area(double x, double y)
+        {
+            Console.WriteLine($"This is the base method calling with 2 parameters");
+        }
+    }
+    public class Circle : Shape
+    {
+        public double Radius { get; set; }
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+        public override void Area()
+        {
+            Console.WriteLine($"The area of the circle is: {3.14 * Math.Pow(Radius, 2)}");
+
+            base.Area();
+        }
+        public override void Area(double radius)
+        {
+            Console.WriteLine($"The area of the circle is: {3.14 * Math.Pow(radius, 2)}");
+        }
+    }
+    public class Square : Shape
+    {
+        private double _length;
+
+        public double Length
+        {
+            get { return _length; }
+            set { _length = value; }
+        }
+
+        public Square() { }
+        public Square(double length)
+        {
+            Length = length;
+        }
+        public override void Area()
+        {
+            Console.WriteLine($"The area of the square is: {Math.Pow(Length,2)}");
+
+            base.Area();
+        }
+    }
+    public class Rectangle: Shape
+    {
+        public double Height { get; set; }
+        public double Width { get; set; }
+        public Rectangle() { }
+        public Rectangle(double height, double width)
+        {
+            Height = height;
+            Width = width;
+        }
+        public override void Area()
+        {
+            Console.WriteLine($"The area of the rectangle is: {Height * Width}");
+            base.Area();
+        }
+        public override void Area(double x)
+        {
+            base.Area(x);
+        }
+        public override void Area(double height, double width)
+        {
+            Console.WriteLine($"The area of the rectangle is: {height * width}");
+        }
+    }
+}
