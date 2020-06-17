49a32d8d437b9c94dd6e28436c3d5acaf7c70825
Andrej Chichakovski <Andrej.Chichakovski@seavus.com>
2020-06-17 15:11:43 +0200

    Exercise solution

---
+++ g5/Class05/Adv.Class03/Adv.Class03.Exercise/Entities/Pet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Adv.Class03.Exercise.Entities
+{
+	public abstract class Pet
+	{
+		public string Name { get; set; }
+		public string Type { get; set; }
+		public int Age { get; set; }
+		public abstract string PrintInfo();
+		public Pet(string name, int age)
+		{
+			Name = name;
+			Age = age;
+		}
+	}
+}
