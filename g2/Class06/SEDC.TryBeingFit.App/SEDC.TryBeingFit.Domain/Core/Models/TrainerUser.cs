e0e3cb4da41da06dd54570ae129ec8ee6e4bfcbb
Martin <panovski.martin93@gmail.com>
2020-06-19 19:56:49 +0200

    Demo code part 1 added

---
+++ g2/Class06/SEDC.TryBeingFit.App/SEDC.TryBeingFit.Domain/Core/Models/TrainerUser.cs
@@ -0,0 +1,28 @@
+﻿using SEDC.TryBeingFit.Domain.Core.Enums;
+using SEDC.TryBeingFit.Domain.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.TryBeingFit.Domain.Core.Models
+{
+    public class TrainerUser : User, ITrainerUser
+    {
+        public int YearsOfExperience { get; set; }
+
+        public TrainerUser()
+        {
+            Role = UserRole.Trainer;
+        }
+
+        public bool ChangeSchedule(ILiveTraining liveTraining)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string Print()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
