5e6805a65a2971fdf6bf7fe0aaed91b02be960b9
Wekoslav Stefanovski <swekster@gmail.com>
2020-06-19 20:04:55 +0200

    Added workshop init

---
+++ g4/06-Fitness/Fitness/Fitness.Models/IApplicationUser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fitness.Models
+{
+    public interface IApplicationUser
+    {
+        int ID { get; set; }
+
+        string FirstName { get; set; }
+
+        string LastName { get; set; }
+
+        string UserName { get; set; }
+
+        string Password { get; set; }
+
+        string FullName { get; }
+
+        bool IsLoggedIn { get; }
+
+        void ShowAccount();
+
+        bool Login();
+
+        void Logoff();
+    }
+}
---
+++ g4/06-Fitness/Fitness/Fitness.Models/Trainer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fitness.Models
+{
+    public class Trainer : IApplicationUser
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string FullName { get; }
+        public bool IsLoggedIn { get; private set; }
+
+        public bool Login()
+        {
+            Console.WriteLine($"[INFO]: Employee {ID} started working in @ {DateTime.Now.ToShortTimeString()}");
+            IsLoggedIn = true;
+            return IsLoggedIn;
+        }
+
+        public void Logoff()
+        {
+            Console.WriteLine($"[INFO]: Employee {ID} stopped working @ {DateTime.Now.ToShortTimeString()}");
+            IsLoggedIn = false;
+        }
+
+        public void ShowAccount()
+        {
+            Console.WriteLine($"EmployeeID: {ID}");
+            Console.WriteLine($"Name: {FullName}");
+        }
+    }
+}
