db6dfa037e22c69eb70564803097d4bc501bbf45
Dejan <djovanov92@gmail.com>
2020-06-19 21:16:22 +0200

    added Db impl and helper services

---
+++ g2/Class06/SEDC.TryBeingFit.App/SEDC.TryBeingFit.Domain/Db/IDb.cs
@@ -0,0 +1,20 @@
+﻿using SEDC.TryBeingFit.Domain.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.TryBeingFit.Domain.Db
+{
+    public interface IDb<T> where T : BaseEntity
+    {
+        List<T> GetAll();
+
+        T GetById(int id);
+
+        int Insert(T entity);
+
+        void RemoveById(int id);
+
+        void Update(T entity);
+    }
+}
