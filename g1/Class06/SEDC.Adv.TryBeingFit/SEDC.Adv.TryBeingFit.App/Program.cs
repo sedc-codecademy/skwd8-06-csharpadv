bc92cd2c9d78a59bddbdb5fd9f5ad67dfe60df8a
ivletest <karakan@protonmail.com>
2020-06-20 14:28:14 +0200

    G1 updated class 6 & 7

---
+++ g1/Class06/SEDC.Adv.TryBeingFit/SEDC.Adv.TryBeingFit.App/Program.cs
@@ -0,0 +1,75 @@
+﻿using SEDC.Adv.TryBeingFit.Domain.Entities;
+using SEDC.Adv.TryBeingFit.Domain.Enums;
+using SEDC.Adv.TryBeingFit.Services.Services;
+using System;
+
+namespace SEDC.Adv.TryBeingFit.App
+{
+    class Program
+    {
+		public static IUIService _uiService = new UIService();
+		public static IUserService<StandardUser> _standardUserSrvc = new UserService<StandardUser>();
+		public static IUserService<PremiumUser> _premiumUserSrvc = new UserService<PremiumUser>();
+		public static IUserService<TrainerUser> _trainerUserSrvc = new UserService<TrainerUser>();
+		public static User _currentUser;
+
+		public static void Seed()
+		{
+			_standardUserSrvc.Register(new StandardUser() { FirstName = "Bob", LastName = "Bobsky", Username = "bobob1", Password = "bobbest1" });
+			_premiumUserSrvc.Register(new PremiumUser() { FirstName = "Jill", LastName = "Wayne", Username = "jilllw", Password = "jillsuper26" });
+			_trainerUserSrvc.Register(new TrainerUser() { FirstName = "John", LastName = "Johnsky", Username = "johnjj", Password = "johny55" });
+		}
+		static void Main(string[] args)
+		{
+			Seed();
+			while (true)
+			{
+				Console.Clear();
+				int loginChoice = _uiService.LogInMenu();
+				Console.Clear();
+				if (loginChoice == 1)
+				{
+					int roleChoice = _uiService.RoleMenu();
+					UserRole role = (UserRole)roleChoice;
+					Console.Clear();
+					Console.WriteLine("Enter username:");
+					string username = Console.ReadLine();
+					Console.WriteLine("Enter password:");
+					string password = Console.ReadLine();
+					switch (role)
+					{
+						case UserRole.Standard:
+							_currentUser = _standardUserSrvc.LogIn(username, password);
+							break;
+						case UserRole.Premium:
+							_currentUser = _premiumUserSrvc.LogIn(username, password);
+							break;
+						case UserRole.Trainer:
+							_currentUser = _trainerUserSrvc.LogIn(username, password);
+							break;
+					}
+
+					if (_currentUser == null) continue;
+					break;
+				}
+				else
+				{
+					StandardUser newUser = new StandardUser();
+					Console.WriteLine("Enter first name:");
+					newUser.FirstName = Console.ReadLine();
+					Console.WriteLine("Enter last name:");
+					newUser.LastName = Console.ReadLine();
+					Console.WriteLine("Enter username:");
+					newUser.Username = Console.ReadLine();
+					Console.WriteLine("Enter password:");
+					newUser.Password = Console.ReadLine();
+					User user = _standardUserSrvc.Register(newUser);
+					if (user == null) continue;
+				}
+			}
+			Console.Clear();
+			_uiService.Welcome(_currentUser);
+			_uiService.MainMenu(_currentUser.Role);
+		}
+	}
+}
---
+++ g1/Class06/SEDC.Adv.TryBeingFit/SEDC.Adv.TryBeingFit.Domain/Entities/TrainerUser.cs
@@ -0,0 +1,28 @@
+﻿using SEDC.Adv.TryBeingFit.Domain.Enums;
+using SEDC.Adv.TryBeingFit.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.Adv.TryBeingFit.Domain.Entities
+{
+    public class TrainerUser : User, ITrainerUser
+    {
+        public int YearsExperience { get; set; }
+
+        public TrainerUser()
+        {
+            Role = UserRole.Trainer;
+        }
+
+        public bool ChangeSchedule(ILiveTraining liveTraining)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string PrintInfo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
