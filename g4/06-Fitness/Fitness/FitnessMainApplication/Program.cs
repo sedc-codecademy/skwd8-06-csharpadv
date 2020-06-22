01d737f003ee5a7fb69037f8f8073f77c5dce8eb
Wekoslav Stefanovski <swekster@gmail.com>
2020-06-22 16:57:40 +0200

    Fitness changes

--- g4/06-Fitness/Fitness/FitnessMainApplication/Program.cs
+++ g4/06-Fitness/Fitness/FitnessMainApplication/Program.cs
@@ -1,6 +1,8 @@
-﻿using Fitness.Service;
+﻿using Fitness.Models;
+using Fitness.Service;
 
 using System;
+using System.Collections.Generic;
 
 namespace FitnessMainApplication
 {
@@ -9,7 +11,11 @@ namespace FitnessMainApplication
         static void Main(string[] args)
         {
             #region init
-            IUsersRepository users = new MemoryUsersRepository();
+            List<IApplicationUser> initials = new List<IApplicationUser>
+            {
+
+            };
+            IUsersRepository users = new MemoryUsersRepository(initials);
             #endregion
 
             // lots of code
