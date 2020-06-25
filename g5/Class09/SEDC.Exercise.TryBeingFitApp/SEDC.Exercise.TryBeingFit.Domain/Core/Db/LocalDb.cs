55b60526da4be404b7a70e332b1e20f2be5d7108
Andrej Chichakovski <Andrej.Chichakovski@seavus.com>
2020-06-25 19:45:58 +0200

    Class09: Be Fit

---
+++ g5/Class09/SEDC.Exercise.TryBeingFitApp/SEDC.Exercise.TryBeingFit.Domain/Core/Db/LocalDb.cs
@@ -0,0 +1,49 @@
+﻿using SEDC.Exercise.TryBeingFit.Domain.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SEDC.Exercise.TryBeingFit.Domain.Core.Db
+{
+    public class LocalDb<T> where T : BaseEntity
+    {
+        public int IdCount { get; set; }
+        private List<T> db;
+
+        public LocalDb()
+        {
+            db = new List<T>();
+            IdCount = 1;
+        }
+
+        public T GetById(int id)
+        {
+            return db.SingleOrDefault(x => x.Id == id);
+        }
+
+        public int Insert(T entity)
+        {
+            entity.Id = IdCount;
+            db.Add(entity);
+            IdCount++;
+            return entity.Id;
+
+        }
+
+        public void RemoveById(int id)
+        {
+            
+        }
+
+        public void Update(T entity)
+        {
+
+        }
+
+        public List<T> GetAll()
+        {
+            return db;
+        }
+    }
+}
---
+++ g5/Class09/SEDC.Exercise.TryBeingFitApp/SEDC.Exercise.TryBeingFit.Domain/Core/Entities/LiveTraining.cs
@@ -0,0 +1,29 @@
+﻿using SEDC.Exercise.TryBeingFit.Domain.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.Exercise.TryBeingFit.Domain.Core.Entities
+{
+    public class LiveTraining : Training, ILiveTraining, IVideoTraining
+    {
+        public DateTime NextSession { get; set; }
+
+        public TrainerUser Trainer { get; set; }
+
+        public string CheckRating()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int HoursToNextSession()
+        {
+            return 0;
+        }
+
+        public override string Print()
+        {
+            return $"[{Difficulty}]{Title} - {Description} with {Trainer.Print()}";
+        }
+    }
+}
