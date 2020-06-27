6414b4833d7908c455d23a16d2e93c8973b4aafc
Trajan Stevkovski <trajan.stevkovski@seavus.com>
2020-06-27 10:28:39 +0200

    added exampe for events

---
+++ g6/Class09/SEDC.Adv.Class09/SEDC.Adv.Class09.Example/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SEDC.Adv.Class09.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var videoStore = new VideoStore(); // publisher - raises the event
+            var mailService = new MailService(); // subscriber - listen to events
+
+            videoStore.NotifyUsers += mailService.SendMail;
+
+            videoStore.AddNewMovie("Terminator :)");
+
+            Console.ReadLine();
+        }
+    }
+}
