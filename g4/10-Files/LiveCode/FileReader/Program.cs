87d591352ac8f34194c389a0e69dad0ff24b0fb6
Wekoslav Stefanovski <swekster@gmail.com>
2020-06-29 19:08:30 +0200

    Exercise 2

--- g4/10-Files/LiveCode/FileReader/Program.cs
+++ g4/10-Files/LiveCode/FileReader/Program.cs
@@ -7,7 +7,17 @@ namespace FileReader
     {
         static void Main(string[] args)
         {
+            var basedir = @"C:\Source\SEDC\skwd8-06-csharpadv";
+            var groupDir = @".\g4\";
+            var classDir = "10-Files";
+
             var filename = @"out.txt";
+
+            Console.WriteLine(basedir + groupDir + "\\"+  classDir + "\\"+ filename);
+            Console.WriteLine(Path.Combine(basedir, groupDir, classDir, filename));
+
+            Console.WriteLine(Path.GetFullPath(filename));
+
             if (File.Exists(filename))
             {
                 var contents = File.ReadAllLines(filename);
@@ -16,13 +26,13 @@ namespace FileReader
                 {
                     Console.WriteLine($">> {line} <<");
                 }
-
             }
             else
             {
                 string[] contents = { "One", "Two", "Three", "Four", "Five" };
                 File.WriteAllLines(filename, contents);
                 Console.WriteLine("Written file");
+                
             }
             
         }
