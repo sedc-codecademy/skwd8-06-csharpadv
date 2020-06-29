7fefa62a32b15c4994c9b9dd94cda49c8e31f0ee
Wekoslav Stefanovski <swekster@gmail.com>
2020-06-29 18:29:07 +0200

    Filereader example

---
+++ g4/10-Files/LiveCode/FileReader/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace FileReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filename = @"out.txt";
+            if (File.Exists(filename))
+            {
+                var contents = File.ReadAllLines(filename);
+                File.Delete(filename);
+                foreach (var line in contents)
+                {
+                    Console.WriteLine($">> {line} <<");
+                }
+
+            }
+            else
+            {
+                string[] contents = { "One", "Two", "Three", "Four", "Five" };
+                File.WriteAllLines(filename, contents);
+                Console.WriteLine("Written file");
+            }
+            
+        }
+    }
+}
