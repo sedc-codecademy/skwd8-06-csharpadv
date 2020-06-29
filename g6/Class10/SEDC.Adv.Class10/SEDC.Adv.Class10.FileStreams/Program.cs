daf9fd46669a4756bf03c3043cd4a8b1e21ba593
Trajan Stevkovski <trajan.stevkovski@seavus.com>
2020-06-29 20:50:26 +0200

    added class 10

---
+++ g6/Class10/SEDC.Adv.Class10/SEDC.Adv.Class10.FileStreams/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+
+namespace SEDC.Adv.Class10.FileStreams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileFolder = "./" + @"testFolderCreatedFromC#\";
+            string filePath = fileFolder + "test.txt";
+
+            if (!Directory.Exists(fileFolder))
+            {
+                Directory.CreateDirectory(fileFolder);
+                Console.WriteLine("Folder is created");
+            }
+
+            if (!File.Exists(filePath))
+            {
+                File.Create(filePath).Close();
+                Console.WriteLine("New file was created");
+            }
+
+            // Writing with stream writer
+            // open stream writer
+            StreamWriter sw1 = new StreamWriter(filePath);
+            sw1.WriteLine("Hello SEDC!");
+            sw1.WriteLine("We are writing from StreamWriter!");
+            Console.WriteLine("Writing is complete");
+            // close stream writer
+            sw1.Close();
+
+            Console.ReadLine();
+
+            // Writing with stream writer in using statement
+            using (StreamWriter swUsing = new StreamWriter(filePath))
+            {
+                swUsing.WriteLine("Hello from using statement");
+                Console.WriteLine("Writing is complete in using statement");
+            }
+
+            // Writing with stream writer without rewriting the previous text
+            using (StreamWriter sw = new StreamWriter(filePath, true))
+            {
+                sw.WriteLine("Hello Again");
+                sw.WriteLine("This is written on top of the previous one!");
+                Console.WriteLine("Writing again!");
+            }
+
+            // Reading with StreamReader
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                string firstLine = sr.ReadLine();
+                string secondLine = sr.ReadLine();
+                string restOfTheFile = sr.ReadToEnd();
+                Console.WriteLine($"First line: {firstLine}");
+                Console.WriteLine($"Second line: {secondLine}");
+                Console.WriteLine($"Rest of the file: {restOfTheFile}");
+            }
+
+            // set position of cursor and read from there
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                // setting the position of the cursor
+                sr.BaseStream.Position = 49;
+                string restOfTheFile = sr.ReadToEnd();
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"Rest of the file: {restOfTheFile}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ g6/Class10/SEDC.Adv.Class10/SEDC.Adv.Class10.FileSystem/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.IO;
+
+namespace SEDC.Adv.Class10.FileSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // absolute path to project folder
+            string absoluthPath = @"C:\SEDC\SEDC08\c#Advanced\skwd8-06-csharpadv\g6\Class10\SEDC.Adv.Class10\SEDC.Adv.Class10.FileSystem\";
+
+            // relative path
+            string relativePath = @"./";
+
+            var currentDirectory = Directory.GetCurrentDirectory();
+            Console.WriteLine(currentDirectory);
+
+
+            #region Create delete folder
+            bool testFolderExists = Directory.Exists(relativePath + "testFolder");
+            Console.WriteLine("Case relative");
+            Console.WriteLine(testFolderExists);
+            bool testFolder2Exists = Directory.Exists(absoluthPath + "bin");
+            Console.WriteLine("Case absolute");
+            Console.WriteLine(testFolder2Exists);
+
+            // creating new folder
+            string newFolderName = "testFolderC#Adv";
+            string newFolderPathAbsolute = 
+                @$"C:\SEDC\SEDC08\c#Advanced\skwd8-06-csharpadv\g6\Class10\SEDC.Adv.Class10\SEDC.Adv.Class10.FileSystem\bin\Debug\netcoreapp3.1\{newFolderName}";
+            // new folder path relative
+            string newFolder = relativePath + "testFolderCreatedFromC#";
+            //Console.WriteLine($"Does testFolderCreatedFromC# exists: {Directory.Exists(newFolder)}");
+            //if (!Directory.Exists(newFolder))
+            //{
+            //    Directory.CreateDirectory(newFolder);
+            //    Console.WriteLine("New folder was created");
+            //}
+
+            //Console.WriteLine($"Does testFolderCreatedFromC# exists: {Directory.Exists(newFolder)}");
+
+            //Console.ReadLine();
+            // deleting folder
+            //if (Directory.Exists(newFolder))
+            //{
+            //    Directory.Delete(newFolder);
+            //    Console.WriteLine("testFolderCreatedFromC# has been deleted");
+            //}
+            //Console.WriteLine("Does folder testFolderCreatedFromC# exists:");
+            //Console.WriteLine(Directory.Exists(newFolder));
+            #endregion
+
+            string fileFolder = relativePath + @"testFolderCreatedFromC#\";
+
+            // Creating directory
+            if (!Directory.Exists(fileFolder))
+            {
+                Directory.CreateDirectory(fileFolder);
+                Console.WriteLine("Folder is created");
+            }
+
+            bool testTxtExists = File.Exists(fileFolder + "test.txt");
+            Console.WriteLine("Does test.txt exists:");
+            Console.WriteLine(testTxtExists);
+
+            // Creating file
+            if(!File.Exists(fileFolder + "test.txt"))
+            {
+                File.Create(fileFolder + "test.txt").Close();
+                Console.WriteLine("File was created");
+            }
+
+            Console.ReadLine();
+
+            //// Deleting file
+            //if(File.Exists(fileFolder + "test2.txt"))
+            //{
+            //    File.Delete(fileFolder + "test2.txt");
+            //    Console.WriteLine("File was deleted");
+            //}
+
+            Console.ReadLine();
+            // content to write in file
+            string text = "Hello SEDC! We are writing in a file";
+            // path for the file
+            string filePath = fileFolder + "test.txt";
+
+            if (File.Exists(filePath))
+            {
+                File.WriteAllText(filePath, text);
+                Console.WriteLine("Successfuly written in a file!");
+            }
+
+            var filesToBeWritten = Directory.GetFiles(fileFolder);
+
+            if (File.Exists(filePath))
+            {
+                File.AppendAllText(filePath, "\n" + text + " Second line");
+                File.AppendAllLines(filePath, filesToBeWritten);
+                Console.WriteLine("Successfuly written in a file!");
+            }
+
+            Console.ReadLine();
+
+            if (File.Exists(filePath))
+            {
+                string content = File.ReadAllText(filePath);
+                Console.WriteLine("This is the content:");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(content);
+                Console.ResetColor();
+            }
+
+            var files = Directory.GetFiles(fileFolder);
+
+            foreach (var file in files)
+            {
+                Console.WriteLine(file);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
