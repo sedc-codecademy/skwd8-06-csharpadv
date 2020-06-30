592fb73a4d2af90abfffd9a60d920d7887dd09c6
dblazeski96 <dblazeski96@gmail.com>
2020-06-30 18:31:49 +0200

    Added recap code for delegates

---
+++ g3/Class-10/EventsAndDelegatesRecal/Delagates/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Delagates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shop myShop = new Shop();
+
+            myShop.Inform += () => { };
+            myShop.Inform += () => { Console.WriteLine("one-line code"); };
+            myShop.Inform += () => 
+            { 
+                Console.WriteLine("multiple-line code"); 
+            };
+            myShop.Inform += StaticMethod;
+
+            myShop.AddMethodToDelegate(StaticMethod);
+
+            myShop.Inform();
+
+            Console.ReadKey();
+        }
+
+        static void StaticMethod()
+        {
+            Console.WriteLine("static method code");
+        }
+    }
+}
