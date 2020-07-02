1be4162d2999dade9a4314a28c93d9078e9908d3
Goce Kabov <Goce.Kabov@seavus.com>
2020-07-02 22:35:13 +0200

    added class 09

---
+++ g1/Class09/ManagingResources/Disposing/CustomReaderWriter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Disposing
+{
+    public class CustomReaderWriter : IDisposable
+    {
+        private string path;
+        private StreamWriter _sw;
+        private bool disposedValue = false;
+
+        public CustomReaderWriter(string filePath)
+        {
+            path = filePath;
+            _sw = new StreamWriter(path, true);
+        }
+
+        public void WriteInFile(string text)
+        {
+            _sw.WriteLine(text);
+        }
+
+
+        public void Dispose()
+        {
+            _dispose(true);
+        }
+
+        private void _dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _sw.Dispose();
+                }
+                path = "";
+                disposedValue = true;
+            }
+        }
+
+    }
+}
---
+++ g1/Class09/ManagingResources/NullableTypes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace NullableTypes
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public int? Score { get; set; }
+        public bool IsEmployed { get; set; }
+        public bool? HasPet { get; set; }
+        public string Name { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            Console.WriteLine(person.Id);
+            Console.WriteLine(person.Score);
+            person.Score = null;
+            person.Name = null;
+            person.HasPet = null;
+            //person.IsEmployed = null;
+            person = null;
+            //person.Id = null;
+        }
+    }
+}
