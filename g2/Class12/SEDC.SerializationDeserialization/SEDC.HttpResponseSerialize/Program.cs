4d4111c2b3bd203137066d27587172f8ddc86692
Martin <panovski.martin93@gmail.com>
2020-07-04 16:10:52 +0200

    Class 12 demo code added part 2

---
+++ g2/Class12/SEDC.SerializationDeserialization/SEDC.HttpResponseSerialize/Program.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using SEDC.HttpResponseSerialize.Models;
+using SEDC.SerializeServices.Services;
+using System;
+using System.Collections.Generic;
+
+namespace SEDC.HttpResponseSerialize
+{
+    class Program
+    {
+        public static void PrintData(List<User> users)
+        {
+            foreach (var user in users)
+            {
+                Console.WriteLine($"Name: {user.Name}");
+                Console.WriteLine($"Username: {user.Username}");
+                Console.WriteLine($"Address: {user.Address.Zip}");
+                Console.WriteLine($"Lat: {user.Address.GeoLocation.Lat}");
+                Console.WriteLine($"Lng: {user.Address.GeoLocation.Lng}");
+                Console.WriteLine("---------------------------------------------");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string url = "https://jsonplaceholder.typicode.com/users";
+            
+            string usersResult = HttpService.GetData(url);
+            List<User> users = JsonConvert.DeserializeObject<List<User>>(usersResult);
+
+            PrintData(users);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
