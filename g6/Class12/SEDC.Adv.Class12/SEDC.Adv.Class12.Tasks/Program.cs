66c0469db544865d61d01c8ca15e81f33c5d49f6
Trajan Stevkovski <trajan.stevkovski@seavus.com>
2020-07-06 20:47:47 +0200

    Added class 12

---
+++ g6/Class12/SEDC.Adv.Class12/SEDC.Adv.Class12.Tasks/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SEDC.Adv.Class12.Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var stopwatch = new Stopwatch();
+
+            //stopwatch.Start();
+
+            ////Task myTask = new Task(() =>
+            ////{
+            ////    Thread.Sleep(3000);
+            ////    Console.WriteLine("Success after 3000 ms");
+            ////});
+
+            ////myTask.Start();
+
+            //Task<int> sumTask = new Task<int>(() => 
+            //{
+            //    Thread.Sleep(5000);
+            //    Console.WriteLine("We can now get the number...");
+            //    return 8;
+            //});
+
+            //sumTask.Start();
+
+            //Console.WriteLine("Enter name: ");
+            //string name = Console.ReadLine();
+
+            //Task.Run(() =>
+            //{
+            //    Thread.Sleep(3000);
+            //    Console.WriteLine("This is executed immedietly");
+            //});
+
+            //Console.WriteLine(name);
+            //Console.WriteLine(sumTask.Result);
+
+            //Console.WriteLine(stopwatch.Elapsed);
+            //stopwatch.Stop();
+
+            //for (int i = 1; i < 20; i++)
+            //{
+            //    int temp = i;
+            //    Task.Run(() =>
+            //    {
+            //        Thread.Sleep(2000);
+            //        Console.WriteLine($"TASK No. {temp}");
+            //    });
+            //}
+
+            Task myTask = Task.Run(() =>
+            {
+
+            });
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ g6/Class12/SEDC.Adv.Class12/SEDC.Adv.Class12.Threads/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace SEDC.Adv.Class12.Threads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var stopwatch = new Stopwatch();
+
+            //stopwatch.Start();
+
+            // long runing tasks
+
+            //Thread myThread = new Thread(() =>
+            //{
+            //    Thread.Sleep(10000);
+            //    Console.WriteLine("Task one complete");
+            //});
+            //myThread.Start();
+
+            //new Thread(() =>
+            //{
+            //    Thread.Sleep(7000);
+            //    Console.WriteLine("Task two complete");
+            //}).Start();
+
+            //new Thread(LongRunningTaskThree).Start();
+
+            //Console.WriteLine("Enter name: ");
+            //string name = Console.ReadLine();
+
+            //Console.WriteLine(name);
+            // --
+
+            //Console.WriteLine(stopwatch.Elapsed);
+            //stopwatch.Stop();
+
+
+            // get value from thread
+
+            int val = 0;
+
+            var myThread = new Thread(() => 
+            {
+                Thread.Sleep(20000);
+                val = 10;
+            });
+            myThread.Start();
+
+            Console.WriteLine("Enter name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Before join");
+            Console.WriteLine(val);
+            Console.WriteLine(name);
+
+            myThread.Join(); // with join we await excution of other thread
+            Console.WriteLine("After join");
+            Console.WriteLine(val);
+
+            Console.ReadLine();
+        }
+
+        public static void LongRunningTaskOne()
+        {
+            Thread.Sleep(10000);
+            Console.WriteLine("Task one complete");
+        }
+
+        public static void LongRunningTaskTwo()
+        {
+            Thread.Sleep(7000);
+            Console.WriteLine("Task two complete");
+        }
+
+        public static void LongRunningTaskThree()
+        {
+            Thread.Sleep(4000);
+            Console.WriteLine("Task three complete");
+        }
+    }
+}
