174ffd4b76bed54af775c8457e440623251e850b
dblazeski96 <dblazeski96@gmail.com>
2020-07-07 22:01:46 +0200

    Added code for serializtion/deserialization

---
+++ g3/Class-13/Class13/RecapApp/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecapApp
+{
+    public struct Product // Change struct to class to see changes
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ g3/Class-13/Class13/SerializationDeserializationApp/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SerializationDeserializationApp
+{
+    public class Car
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+
+        public override string ToString()
+        {
+            //return typeof(Car).FullName; // Default behaviour of ToString()
+            //return $"Brand: {Brand}; Model: {Model}; Year: {Year}; Color: {Color}"; // Custom properties string
+            return "{" +
+                "  \"brand\": " + "\"" + Brand + "\"," +
+                "  \"model\": " + "\"" + Model + "\"," +
+                "  \"year\": " + "\"" + Year + "\"," +
+                "  \"color\": " + "\"" + Color + "\"" +
+                "}";
+        }
+    }
+}
