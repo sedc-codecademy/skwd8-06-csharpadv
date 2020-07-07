7f3ea1ccc5c2a6314c8ed2219526b8c8d34be89f
ivletest <karakan@protonmail.com>
2020-07-07 21:09:08 +0200

    Async programming

---
+++ g1/Class 12/SEDC.AsyncProgramming/SEDC.AsyncProgramming.Test/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+
+namespace SEDC.AsyncProgramming.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            int j = 2;
+
+            int result = i + j;
+        }
+    }
+}
---
+++ g1/Class 12/SEDC.AsyncProgramming/SEDC.AsyncProgramming.Threads/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+
+namespace SEDC.AsyncProgramming.Threads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //PrintMessages();
+            PrintMessagesWithThreads();
+        }
+
+        public static void PrintMessages()
+        {
+            Console.WriteLine("Get Ready.");
+            Thread.Sleep(2000);
+            Console.WriteLine("First message.");
+            Thread.Sleep(2000);
+            Console.WriteLine("Second message.");
+            Console.ReadLine();
+        }
+
+        public static void PrintMessagesWithThreads()
+        {
+            Console.WriteLine("Get Ready.");
+            
+            Thread threadOne = new Thread(() => {
+                Thread.Sleep(2000);
+                Console.WriteLine("First message.");
+            });
+
+            new Thread(() =>
+            {
+                Thread.Sleep(2000);
+                Console.WriteLine("Second message.");
+            }).Start();
+
+            Thread.Sleep(1000);
+            threadOne.Start();
+            Console.ReadLine();
+        }
+    }
+}
