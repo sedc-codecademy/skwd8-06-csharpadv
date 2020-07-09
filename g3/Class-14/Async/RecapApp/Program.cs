495b6bc3202f30611189b913ef9723cc740f25e2
dblazeski96 <dblazeski96@gmail.com>
2020-07-09 19:43:15 +0200

    Added Threads code

---
+++ g3/Class-14/Async/RecapApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using Newtonsoft.Json;
+
+namespace RecapApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User dejanUser = new User { Id = 1, Username = "dejan", Password = "trajanBabus" };
+            User trajanUser = new User { Id = 2, Username = "trajan", Password = "dekiBabus" };
+            User viktorijaUser = new User { Id = 3, Username = "viktorija", Password = "123456" };
+
+            List<User> users = new List<User> { dejanUser, trajanUser, viktorijaUser };
+
+            string usersJson = JsonConvert.SerializeObject(users);
+
+            //Console.WriteLine(usersJson);
+
+            List<User> usersDeserialized = JsonConvert.DeserializeObject<List<User>>(usersJson);
+
+            foreach (User user in usersDeserialized)
+            {
+                Console.WriteLine(user.Username);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ g3/Class-14/Async/Threads/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Threads
+{
+    class Program
+    {
+        //setTimeout(() => console.log("test"), 1000);
+
+        static void Main(string[] args)
+        {
+            #region starting stopwatch
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            #endregion
+
+            Console.WriteLine("Strarting app");
+
+
+            //// Sync way of execution
+            //Thread.Sleep(2000);
+            //Console.WriteLine("This is my first message");
+
+            //Thread.Sleep(3000);
+            //Console.WriteLine("This is my second massage");
+            //// About 5100 ms
+
+
+
+            // Async way of execution
+            Thread task1 = new Thread(() =>
+            {
+                Stopwatch task1Stopwatch = Stopwatch.StartNew();
+
+                Thread.Sleep(2000);
+                Console.WriteLine("This is my first message");
+
+                task1Stopwatch.Stop();
+                Console.WriteLine(task1Stopwatch.ElapsedMilliseconds);
+            });
+            task1.Start();
+
+            Thread task2 = new Thread(() =>
+            {
+                Stopwatch task2Stopwatch = Stopwatch.StartNew();
+
+                Thread.Sleep(3000);
+                Console.WriteLine("This is my second massage");
+
+                task2Stopwatch.Stop();
+                Console.WriteLine(task2Stopwatch.ElapsedMilliseconds);
+
+                #region stopping stopwatch
+                stopwatch.Stop();
+                Console.WriteLine($"All time: {stopwatch.ElapsedMilliseconds}");
+                #endregion
+            });
+            task2.Start();
+
+
+            //#region stopping stopwatch
+            //stopwatch.Stop();
+            //Console.WriteLine(stopwatch.ElapsedMilliseconds);
+            //#endregion
+
+            Console.ReadKey();
+        }
+    }
+}
