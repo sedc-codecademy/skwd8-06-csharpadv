4d52515dc15ee5f9bc91b9f3849e58b7886b6dec
Goce Kabov <Goce.Kabov@seavus.com>
2020-07-09 21:52:46 +0200

    added class 13

---
+++ g1/Class13/AsyncProgramming/AsyncCalculator/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // var result = CalculateAsync().Result;
+            //CalculateThree(result);
+            var result = CalculateAsync();
+            var res = result.GetAwaiter();
+            res.OnCompleted(() =>
+            {
+                Console.WriteLine("ASD");
+            });
+
+            Console.ReadLine();
+        }
+
+        //public static async void GrandParent()
+        //{
+        //    await CalculateAsync();
+        //}
+
+        //public static async Task<int> CalculateAsync()
+        //{
+        //    var result = await Task.Run(() =>
+        //    {
+        //        return CalculateOne();
+        //    });
+
+        //    return CalculateTwo(result);
+        //}
+
+        public static async Task CalculateAsync()
+        {
+            var result = await Task.Run(() =>
+            {
+                return CalculateOne();
+            });
+
+            CalculateTwo(result);
+        }
+
+        public static int CalculateOne()
+        {
+            Thread.Sleep(2000);
+            Console.WriteLine("Calculating in method 1");
+            return 100;
+        }
+
+        public static int CalculateTwo(int number)
+        {
+            Console.WriteLine($"Calculating in method 2 with {number}");
+            return 200;
+        }
+
+        public static int CalculateThree(int number)
+        {
+            Console.WriteLine("Calculating in method 3");
+            return 300;
+        }
+    }
+}
---
+++ g1/Class13/AsyncProgramming/FileHandler/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FileHandler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter five words:");
+            List<string> userChoise = new List<string>();
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Word {i+1}: ");
+                userChoise.Add(Console.ReadLine());
+            }
+
+            // Start the HandleFileAsync method
+            Task<int> task = HandleFileAsync(userChoise);
+
+            Console.WriteLine("Please wait patiently while file is processing");
+
+            // Do something at same time as the file is being created and read
+            //Console.WriteLine("Please enter something while file is processing");
+            //string userInput = Console.ReadLine();
+            //Console.WriteLine($"You entered (asynchronious): {userInput}");
+            var result = task.Result;
+            Console.WriteLine($"Count: {result}");
+            Console.WriteLine("[DONE]");
+            // Console.ReadLine();
+        }
+
+        static async Task<int> HandleFileAsync(List<string> textList)
+        {
+            Console.WriteLine("----- Start Working on file ------");
+            string currentDirectory = Directory.GetCurrentDirectory();
+            string directoryPath = Path.Combine(currentDirectory, "FileSystem");
+            string filePath = Path.Combine(directoryPath, "OurFile.txt");
+
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+
+            if (!File.Exists(filePath))
+            {
+                File.Create(filePath).Close();
+            }
+
+            using (StreamWriter sw = new StreamWriter(filePath))
+            {
+                foreach (var str in textList)
+                {
+                    sw.Write(str);
+                }
+            }
+
+            int count = 0;
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                Thread.Sleep(3000);
+                string text = await sr.ReadToEndAsync();
+                count += text.Length;
+            }
+
+            Console.WriteLine("------- File exit --------");
+            return count;
+        }
+    }
+}
