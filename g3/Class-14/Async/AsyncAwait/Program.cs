7be26f7404cdde0980013873b98cd6dea00be357
dblazeski96 <dblazeski96@gmail.com>
2020-07-09 21:50:10 +0200

    Added code for Tasks, Async/Await and WinFormsApp

---
+++ g3/Class-14/Async/AsyncAwait/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsyncAwait
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await InitialExecution();
+
+            Console.ReadLine();
+        }
+
+        public static async Task InitialExecution()
+        {
+            Console.WriteLine(await SendMessage("my first message", 2000));
+            Console.WriteLine(await SendMessage("my second message", 3000));
+
+            PrintMessage("my fourth message", 2000);
+            PrintMessage("my fifth message", 2000);
+        }
+
+        public static async Task<string> SendMessage(string message, int ms)
+        {
+            await Task.Delay(ms);
+            return $"[RECEIVED] {message}";
+        }
+
+        public static async Task PrintMessage(string message, int ms)
+        {
+            await Task.Delay(ms);
+            Console.WriteLine($"[PRINT] {message}");
+        }
+    }
+}
---
+++ g3/Class-14/Async/Task/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region starting stopwatch
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            #endregion
+
+            Console.WriteLine("Staring Tasks app");
+
+            Task task1 = new Task(() =>
+            {
+                Thread.Sleep(2000);
+                Console.WriteLine("My first message");
+            });
+            task1.Start();
+
+            Task<string> task2 = new Task<string>(() =>
+            {
+                Thread.Sleep(3000);
+                //Console.WriteLine("My second massage");
+                return "My second massage";
+            });
+            task2.Start();
+
+            //while (true)
+            //{
+            //    if (task2.IsCompleted)
+            //    {
+            //        Console.WriteLine(task2.Result);
+            //        break;
+            //    }
+            //}
+            Console.WriteLine(task2.Result);
+
+            Task.Run(() =>
+            {
+                Console.WriteLine("My fourth message");
+            });
+            
+
+
+            #region stopping stopwatch
+            while (true)
+            {
+                if (task1.IsCompleted && task2.IsCompleted)
+                {
+                    stopwatch.Stop();
+                    Console.WriteLine($"All time {stopwatch.ElapsedMilliseconds}");
+
+                    break;
+                }
+            }
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+}
