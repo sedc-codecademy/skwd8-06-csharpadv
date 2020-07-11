3aa15c06cc374698321ccd78c22a88e95cd1895f
Wekoslav Stefanovski <swekster@gmail.com>
2020-07-11 11:20:04 +0200

    Added examples

---
+++ g4/14-SOLID/Adv.Class12/GoodBad/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GoodBad
+{
+    class Person {
+        public string Name { get; set; } = string.Empty;
+
+        public static Person EmptyPerson = new Person { Name = string.Empty };
+    }
+
+    class Program
+    {
+        static Random random = new Random();
+
+        static void Main(string[] args)
+        {
+            int? nulint = (random.NextDouble() > 0.5) ? (int?)10 : null;
+
+            // Console.WriteLine(nulint.GetValueOrDefault(-1));
+            Console.WriteLine(nulint ?? -1);
+
+            List<Person> p = GetPersons();
+            
+            var firstPerson = p.FirstOrDefault() ?? Person.EmptyPerson;
+
+            //if (firstPerson == null)
+            //{
+            //    throw new ApplicationException("There are no persons in the GetPersons call");
+            //}
+
+            Console.WriteLine(firstPerson.Name);
+
+        }
+
+        private static List<Person> GetPersons()
+        {
+            //return null;
+            return new List<Person>();
+        }
+    }
+}
---
+++ g4/14-SOLID/Adv.Class12/Props/Parking.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Props
+{
+    class Parking
+    {
+        private int __occupied;
+        private int __capacity;
+
+        public int Capacity { 
+            get => __capacity;
+            set => __capacity = value;
+        }
+
+        public int Occupied
+        {
+            get => __occupied;
+            set {
+                if (value > __capacity)
+                {
+                    throw new ArgumentOutOfRangeException("Unable to park more cars than capacity");
+                }
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Unable to park negative cars");
+                }
+                __occupied = value;
+            }
+        }
+
+        public int Free { }
+    }
+}
