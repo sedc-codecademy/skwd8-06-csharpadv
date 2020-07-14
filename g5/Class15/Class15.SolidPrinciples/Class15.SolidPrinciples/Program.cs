3d2f375815881280d986204a9b8b6596985bd63c
Andrej Chichakovski <Andrej.Chichakovski@seavus.com>
2020-07-14 20:48:44 +0200

    Class15 : Solid Principles

---
+++ g5/Class15/Class15.SolidPrinciples/Class15.SolidPrinciples/Program.cs
@@ -0,0 +1,22 @@
+﻿using Class15.SolidPrinciples.Principles;
+using System;
+
+namespace Class15.SolidPrinciples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // SOLID
+
+            LengthStringComparing strComp2 = new LengthStringComparing("Bob", "John");
+            strComp2.Compare();
+
+            LettersStringComparingGood strComGoood = new LettersStringComparingGood("Bob", "Jill");
+            strComGoood.Compare();
+
+        }
+    }
+}
